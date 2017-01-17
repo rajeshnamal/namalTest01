15ef340fedd88a5dfb27fd7528df63f500c8d330
rajeshnamal <rajeshnamal@gmail.com>
2017-01-17 12:20:57 +0530

    asd
    sad

---
+++ WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Class1
+    {
+    }
+
+    public class clsUser
+    {
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    }
+
+    [DataContract]
+    public class MessageClass
+    {
+        [DataMember]
+        public string message { get; set; }
+
+        [DataMember]
+        public string status { get; set; }
+        [DataMember]
+        public int Active { get; set; }
+
+        public enum statusmsg
+        {
+            error, success
+
+        }
+    }
+
+    [DataContract]
+    public class clsCustomer
+    {
+        [DataMember]
+        public int CustomerId { get; set; }
+        [DataMember]
+        public string CardNo { get; set; }
+        [DataMember]
+        public string RegDate { get; set; }
+        [DataMember]
+        public string CustomerName { get; set; }
+        [DataMember]
+        public string ScreenName { get; set; }
+        [DataMember]
+        public string AddressI { get; set; }
+        [DataMember]
+        public string AddressII { get; set; }
+        [DataMember]
+        public string TelNo { get; set; }
+        [DataMember]
+        public string MobileNo { get; set; }
+        [DataMember]
+        public string NICType { get; set; }
+        [DataMember]
+        public string NICNo { get; set; }
+        [DataMember]
+        public string Status { get; set; }
+        [DataMember]
+        public Int16 LocationID { get; set; }
+        [DataMember]
+        public string Location { get; set; }
+        [DataMember]
+        public int UserID { get; set; }
+        [DataMember]
+        public string Remark { get; set; }
+        [DataMember]
+        public string BLockYN { get; set; }
+    }
+
+    [DataContract]
+    [KnownType(typeof(clsPayment))]
+    public class clsPayment
+    {
+        [DataMember]
+        public int PaymentID { get; set; }
+        [DataMember]
+        public string PaymentDate { get; set; }
+
+        [DataMember]
+        public int CustomerID { get; set; }
+        [DataMember]
+        public int LoanNo { get; set; }
+        [DataMember]
+        public int UserID { get; set; }
+        [DataMember]
+        public double Pay_Amount { get; set; }
+        [DataMember]
+        public int AgentID { get; set; }
+        [DataMember]
+        public string CardNo { get; set; }
+    }
+
+
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //WebClient client = new WebClient();
+            //client.Headers["Content-type"] = "application/json";
+            //clsUser oUser = new clsUser() { Username = "namal", Password = "123" };
+            //MemoryStream stream = new MemoryStream();
+            //DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(clsUser));
+            //serializer.WriteObject(stream, oUser);
+
+            //byte[] data = client.UploadData("http://124.43.22.231:8080/AthugalaService.svc/Login", "POST", stream.ToArray());
+
+            //stream = new MemoryStream(data);
+            //serializer = new DataContractJsonSerializer(typeof(MessageClass));
+            //MessageClass result = (MessageClass)serializer.ReadObject(stream);
+
+
+            //string xx = result.message.ToString();
+            //MessageBox.Show(xx);
+            Pyamrnt();
+        }
+
+
+        void Pyamrnt()
+        {
+            WebClient client = new WebClient();
+            client.Headers["Content-type"] = "application/json";
+            clsPayment oPayment = new clsPayment() { CardNo = "000001", CustomerID = 1, LoanNo =3, PaymentDate = "07-31-2016", Pay_Amount = 200, UserID = 1, AgentID = 1 };
+            MemoryStream stream = new MemoryStream();
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(clsPayment));
+            serializer.WriteObject(stream, oPayment);
+
+           // byte[] data = client.UploadData("http://124.43.22.231:8080/AthugalaService.svc/PaymentRecevied", "POST", stream.ToArray());
+            byte[] data = client.UploadData("http://localhost:45197/AthugalaService.svc/PaymentRecevied", "POST", stream.ToArray());
+        
+            stream = new MemoryStream(data);
+            serializer = new DataContractJsonSerializer(typeof(MessageClass));
+            MessageClass result = (MessageClass)serializer.ReadObject(stream);
+
+
+            string xx = result.message.ToString();
+            MessageBox.Show(xx);
+        }
+    }
+}
